c3f4dc86dcbcfeb68986e93e78634193938a23e2
Fadhil-Humam <140069106+Fadhil-Humam@users.noreply.github.com>
2023-08-18 14:07:43 +0700

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player; // untuk mengakses game object Player
+    private Vector3 offset; // membuat variabel bertipe Vector3 bernama offset
+                            // private karena cuma ingin dipakai di skrip ini
+                            // Use this for initialization
+    void Start()
+    { // Offset value: selisih/jarak antara object Player & kamera.
+      //  berarti offset = posisi kamera saat ini - posisi player.
+        offset = transform.position - Player.transform.position;
+    }
+    // LateUpdate is called after all Update functions have been called
+    void LateUpdate()
+    { // mengeset posisi kamera sejauh offset dari object Player
+        transform.position = Player.transform.position + offset;
+    }
+}
